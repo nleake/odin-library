bc3878b9de96e2d5f20c802624293523518d7f29
nleake <6044084+nleake@users.noreply.github.com>
2025-01-21 20:39:22 -0500

    refactor to use classes instead of factoroy functions

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-console.log('hi');
-
 const bookTitle = document.getElementById("booktitle");
 const bookAuthor = document.getElementById("bookauthor");
 const bookLength = document.getElementById("booklength");
 const btn = document.querySelector("button");
 const books = document.getElementById("books");
 
-//constructor function
-function Book(title, author, length) {
-  this.title = title;
-  this.author = author;
-  this.length = length;
+class Book {
+  constructor(title='Unknown', author='Unknown', length='0') {
+    this.title = title;
+    this.author = author;
+    this.length = length;
+  }
 }
+
+class Library {
+  constructor() {
+    this.books = [
+      { title: 'War and Peace', author: 'Leo Tolstoy', length: 1392 },
+      { title: 'Starship Troopers', author: 'Robert A. Heinlein', length: 264 },
+      { title: 'East of Eden', author: 'John Steinbeck', length: 601 },
+      { title: 'Nevernight', author: 'Jay Kristoff', length: 448 },
+    ];
+  }
+
+  addBook(newBook) {
+    this.books.push(newBook);
+  }
+
+}
+
+const library = new Library();
+
 
 function showLibrary() {
   books.innerHTML = "";
-  myLibrary.forEach((book, index) => {
+  library.books.forEach((book, index) => {
     const bookCard = document.createElement("div");
     bookCard.classList.add("bookcard");
     bookCard.innerHTML = `
@@ -37,25 +55,16 @@ function showLibrary() {
 
     const remove = bookCard.querySelector(".remove");
     remove.addEventListener("click", () => {
-      myLibrary.splice(index, 1);
+      library.books.splice(index, 1);
       showLibrary();
     });
   })
 }
 
-const myLibrary = [
-  { title: 'War and Peace', author: 'Leo Tolstoy', length: 1392 },
-  { title: 'Starship Troopers', author: 'Robert A. Heinlein', length: 264 },
-  { title: 'East of Eden', author: 'John Steinbeck', length: 601 },
-  { title: 'Nevernight', author: 'Jay Kristoff', length: 448 },
-];
-
-
-
 const addBookToLibrary = function (event) {
   event.preventDefault();
   const newBook = new Book(bookTitle.value, bookAuthor.value, bookLength.value);
-  myLibrary.push(newBook);
+  library.addBook(newBook);
   bookTitle.value = "";
   bookAuthor.value = "";
   bookLength.value = "";
