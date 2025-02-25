bee1ec0f2abf4f3a78f6f7c397f086fa9c7945e8
nleake <6044084+nleake@users.noreply.github.com>
2025-02-25 12:42:13 -0500

    move methods inside Library class

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
+const btn = document.querySelector("button");
+const books = document.getElementById("books");
 const bookTitle = document.getElementById("booktitle");
 const bookAuthor = document.getElementById("bookauthor");
 const bookLength = document.getElementById("booklength");
-const btn = document.querySelector("button");
-const books = document.getElementById("books");
 
 class Book {
-  constructor(title='Unknown', author='Unknown', length='0') {
+  constructor(title = 'Unknown', author = 'Unknown', length = '0') {
     this.title = title;
     this.author = author;
     this.length = length;
@@ -26,52 +27,47 @@ class Library {
     this.books.push(newBook);
   }
 
+  showLibrary() {
+    books.innerHTML = "";
+    library.books.forEach((book, index) => {
+      const bookCard = document.createElement("div");
+      bookCard.classList.add("bookcard");
+      bookCard.innerHTML = `
+        <h2>${book.title}</h2>
+        <p>by ${book.author}<p>
+        <p class="length">${book.length} pages<p>
+        <div class="buttons">
+          <button class="read" data-index="${index}">Read: ${book.read ? 'Yes' : 'No'}</button>
+          <button class="remove" data-index="${index}">Remove</button>
+        </div>
+      `;
+      books.appendChild(bookCard);
+
+      const read = bookCard.querySelector(".read");
+      read.addEventListener("click", () => {
+        book.read = !book.read;
+       this.showLibrary();
+      });
+
+      const remove = bookCard.querySelector(".remove");
+      remove.addEventListener("click", () => {
+        library.books.splice(index, 1);
+        this.showLibrary();
+      });
+    })
+  }
 }
 
 const library = new Library();
+btn.addEventListener('click', addBookToLibrary);
+library.showLibrary();
 
-
-function showLibrary() {
-  books.innerHTML = "";
-  library.books.forEach((book, index) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("bookcard");
-    bookCard.innerHTML = `
-      <h2>${book.title}</h2>
-      <p>by ${book.author}<p>
-      <p class="length">${book.length} pages<p>
-      <div class="buttons">
-        <button class="read" data-index="${index}">Read: ${book.read ? 'Yes' : 'No'}</button>
-        <button class="remove" data-index="${index}">Remove</button>
-      </div>
-    `;
-    books.appendChild(bookCard);
-
-    const read = bookCard.querySelector(".read");
-    read.addEventListener("click", () => {
-      book.read = !book.read;
-      showLibrary();
-    });
-
-    const remove = bookCard.querySelector(".remove");
-    remove.addEventListener("click", () => {
-      library.books.splice(index, 1);
-      showLibrary();
-    });
-  })
-}
-
-const addBookToLibrary = function (event) {
+function addBookToLibrary(event) {
   event.preventDefault();
   const newBook = new Book(bookTitle.value, bookAuthor.value, bookLength.value);
   library.addBook(newBook);
   bookTitle.value = "";
   bookAuthor.value = "";
   bookLength.value = "";
-  showLibrary();
+  library.showLibrary();
 }
-
-btn.addEventListener('click', addBookToLibrary);
-showLibrary();
-
-
